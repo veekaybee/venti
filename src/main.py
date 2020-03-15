e21f3417c909e13eeab60373ee08311702f0e819
Vicki <vicki.boykis@gmail.com>
2020-03-15 13:23:29 -0400

    refactor dockerfile

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import json
 import random
 import streamlit as st
 
+st.markdown("## Refresh to create a new thinkpiece.")
+
 image = Image.open('src/venti.png')
 st.image(image, use_column_width=True)
 
@@ -12,6 +14,6 @@ with open('generated_phrases.json', 'r') as generated_json:
     phrase = random.choice(list(data.values()))
     st.markdown(str(phrase))
 
-st.markdown("## Refresh to see a new thought leader post.")
-st.markdown("Made with 💖 by (Vicki Boykis)[http:/www.vickiboykis.com]")
-st.markdown("More about the project (here)[http:/www.vickiboykis.com]")
+
+st.markdown("Made with 💖 by [Vicki Boykis](http://www.vickiboykis.com)")
+st.markdown("More about the project [here](http://www.vickiboykis.com)")
